9cb29abf9cd5fb309ddc3404969b464e9d7f882f
moneyan9 <m.tanimura1226@gmail.com>
2020-11-30 21:44:06 +0900

    tutorial32 finished

--- pages/api/people.ts
+++ pages/api/people.ts
@@ -1,3 +1,5 @@
+import { decode } from 'jsonwebtoken';
+
 import { openDB } from '../../openDB';
 import handler from '../../handler';
 
@@ -5,23 +7,40 @@ export default handler.get(
     async (req, res) => {
         const db = await openDB();
         const people = await db.all('select * from person');
-        res.status(200).json(people);
+
+
+        const decoded = decode(req.headers.authorization!);
+        console.log(decoded);
+
+        res.status(200).json(
+            {
+                id: decoded.id,
+                name: decoded.name,
+                people
+            }
+        );
     }
 ).post(
-    (req, res) => {
-        //何かしらのエラーを投げる
-        throw new Error('method is Not Allowed !');
-    }
-    /*
-    async (req,res)=>{
+
+    async (req, res) => {
+
+        //JWT内のユーザid情報で処理制御
+        const decoded = decode(req.headers.authorization!);
+        if (+decoded.id !== 9999) {
+            res.status(401).json(
+                { message: 'sorry you are not 9999' }
+            );
+
+            return; //後続処理はしないのでreturnする
+        }
+
         const db = openDB();
-        const {lastID} = await (await db).run(
+        const { lastID } = await (await db).run(
             'INSERT INTO Person (name,details,vgroupId) values(?,?,?)',
             req.body.name,
             req.body.details,
             req.body.vgroupId
         );
-        res.status(201).json({...req.body,id:lastID});
+        res.status(201).json({ ...req.body, id: lastID });
     }
-    */
-);
+)
